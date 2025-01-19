1da87ef87a8a6a741fc6d21362955fc0f365c445
maainul <mainul080@yahoo.com>
2025-01-19 09:46:49 +0600

    update for test

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get("/", async(req,res)=>{
+app.get("/", async (req, res) => {
     return res.json({
-        status:201,
-        body:'Home Message'
-    })
-})
+        status: 201,
+        body: "Home Message",
+    });
+});
 
-app.get("/f1", async(req,res)=>{
+app.get("/f1", async (req, res) => {
     return res.json({
-        status:201,
-        body:'f1 Message'
-    })
-})
+        status: 201,
+        body: "f1 Message",
+    });
+});
 
-
-app.get("/get", async(req,res)=>{
+app.get("/get", async (req, res) => {
     return res.json({
-        status:201,
-        body:'Get Message'
-    })
-})
-app.listen(8000,()=>{
-    console.log(`Server running on PORT = 8000. `)
-})
+        status: 201,
+        body: "Get Message",
+    });
+});
+
+module.exports = app; // Export only the app
