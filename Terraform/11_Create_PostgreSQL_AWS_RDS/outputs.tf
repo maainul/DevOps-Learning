afa50eeb9db701ac7379958169382d111f56415e
maainul <mainul080@yahoo.com>
2025-01-25 18:09:35 +0600

    postgres

---
+++ Terraform/11_Create_PostgreSQL_AWS_RDS/outputs.tf
@@ -0,0 +1,11 @@
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = ""
+  sensitive   = true
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = ""
+  sensitive   = true
+}
---
+++ Terraform/11_Create_PostgreSQL_AWS_RDS/versions.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hasicorp/aws"
+      version = ">=3.20.0"
+    }
+  }
+}
